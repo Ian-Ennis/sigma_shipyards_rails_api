0accb1ab29da49df26166cbd3270e869f93f3918
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-31 20:14:04 -0700

    added users/auth controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile' 
+    end
+  end
+
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
