429879ad254ee791b30f847a0a1fc31c1fd82af2
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-02-08 20:23:27 -0700

    stuck on password can't be blank. Invalid user

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,17 +4,19 @@ class AuthController < ApplicationController
     # allows existing users to login (become authorized)
 
     def login 
+        puts "======entering login (auth controller)"
         @user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
                 payload = {user_id: user.id}
                 token = encode_token(payload)
-                render json: {user: user, jwt: token, success: "Welcome back, #{user.username}"}
+                render json: {user: user, jwt: token, success: "Hello, #{user.username}"}
         else 
             render json: {failure: "Log in failed! Username or password invalid. (from auth controller)"}
         end
     end
 
     def auto_login
+        puts "======entering auto_login (auth controller)"
         if session_user
             render json: session_user
         else
@@ -23,6 +25,7 @@ class AuthController < ApplicationController
     end
 
     def user_is_authed
+        puts "=======entering user_is_authed (auth controller)"
         render json: {message: "You have been authorized"}
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,28 @@
 class UsersController < ApplicationController
+    skip_before_action :require_login, only: [:create]
 
       # handles user signing up
     def create 
-        @user = User.create(user_params)
+        puts "===entering users create"
+        user = User.create(user_params)
+        puts "===user: #{user}"
         if (user.valid?) 
+            puts "yes"
             payload = {user_id: user.id}
             token = encode_token(payload)
-            render json: {user: user, jwt: token}
+            render json: {user: user, include: [jwt: token]}, status: :ok
         else 
+            puts "no"
             render json: {errors: user.errors.full_messages}, status: :not_acceptable
+        end
+        puts "===leaving users create"
     end
 
     private
 
     def user_params
+        puts "===entering user_params"
         params.permit(:username, :password)
+        puts "===leaving user_params"
     end
 end
